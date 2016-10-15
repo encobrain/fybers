04c2c65d0eacd92ec9c530db3f88c13ccfc6bacb
encobrain <encobrain@gmail.com>
2016-10-15 13:11:53 +0300

    Final code of USE

--- USE.js
+++ USE.js
@@ -16,7 +16,7 @@ function* sum_ (a, b) {
 function cb (err, result) {
     console.log('cb used');
 
-    if (err) throw err;
+    if (err) return console.log('Error:', err);
 
     console.log(result);
 }
@@ -27,7 +27,7 @@ function cb (err, result) {
 
 sum_(3,4).run(); // nothing log
 
-sum_(5,6).run(cb); // log result
+sum_(5,6).run(cb); // log result: 11
 
 
 // Use with another generator functions
@@ -37,10 +37,10 @@ function* cube_ (num) {
 }
 
 function* calc_ (a, b) {
-    return (yield cube_(a)) + (yield cube_(b));
+    return (yield* cube_(a)) + (yield* cube_(b));
 }
 
-calc_(2,3).run(cb);
+calc_(2,3).run(cb); // log result: 35
 
 
 // Use with object + generator functions
@@ -60,9 +60,9 @@ function* getName_ (user) {
 
 var user = new User('TestName');
 
-user.getName_().run(cb);  // dirrect get
+user.getName_().run(cb);  // dirrect get // log result: TestName
 
-getName_(user).run(cb);   // from another generator
+getName_(user).run(cb);   // from another generator // log result: TestName
 
 // Use with dirrect callbacks in fiber
 
@@ -78,5 +78,27 @@ function* timeoutCalc_ (time, a, b) {
     return a + b;
 }
 
-timeoutCalc_(1000, 3, 5).run(cb);
+timeoutCalc_(1000, 3, 15).run(cb);
+
+// Throw error thrown 3 callback
+
+function* do1_ () {
+    try {
+        yield* do2_();
+    } catch (err) {
+        console.log('Catched error in do1:', err)
+    }
+
+    return 'Catched error';
+}
+
+function* do2_ () {
+    yield* do3_();
+}
+
+function* do3_ () {
+    throw new Error('Do3 error');
+}
+
+do1_().run(cb);
 
