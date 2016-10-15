41ce789c6909375106def8daed706f224afde01f
encobrain <encobrain@gmail.com>
2016-10-15 13:21:50 +0300

    Added sleep_ addon

---
+++ addons/sleep.js
@@ -0,0 +1,19 @@
+/**
+ * Created by encobrain on 15.10.16.
+ */
+
+var fcb = require('../index').cb;
+
+function* sleep_ (timeout_ms) {
+    var now = Date.now(),
+        cb = fcb()
+        ;
+
+    function next () {
+        cb(null, Date,now() - now);
+    }
+
+    setTimeout(next, timeout_ms);
+}
+
+module.exports.sleep_ = sleep_;
