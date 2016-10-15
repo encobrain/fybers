f1c1987f46a4888553279a8ea7d85aa27b0d001e
encobrain <encobrain@gmail.com>
2016-10-15 20:48:56 +0300

    Added immediate run

--- index.js
+++ index.js
@@ -1,65 +1,9 @@
-var global = (function () { return this; })(),
-    _Error = global.Error,
-    stackRe = new RegExp ('\\n^\\s+at next \\(native\\)|\\n\\s+at[^(]+\\(' + __filename + ':\\d+:\\d+\\)','gm'),
-    generator = (function*(){})(),
-    generatorPrototype = generator.constructor.prototype,
-    _next = generatorPrototype.next,
-    _throw =generatorPrototype.throw,
-    currGen = null
-    ;
+/**
+ * Created by encobrain on 15.10.16.
+ */
 
-function Error (msg) {
-    var error = new _Error(msg);
-
-    error.stack = error.stack.replace(stackRe, '');
-
-    return error;
+module.exports = {
+    cb: require('./lib/index').cb,
+    sleep: require('./lib/addons/sleep'),
+    future: require('./lib/addons/future')
 }
-
-Error.prototype = _Error.prototype;
-
-global.Error = Error;
-
-function cb () {
-    var gen = currGen;
-
-    return function (err,res) {
-        err != null ? gen.throw(err) : gen.next(res);
-    };
-}
-
-
-function next () {
-    currGen = this;
-
-    try {
-        var ret = _next.apply(this, arguments);
-    } catch (err) { this.__cb(err) }
-
-    return ret && ret.done ? this.__cb(null, ret.value) : ret;
-}
-
-function throww () {
-    currGen = this;
-
-    try {
-        var ret = _throw.apply(this, arguments);
-    } catch (err) { this.__cb(err) }
-
-    return ret && ret.done ? this.__cb(null, ret.value) : ret;
-}
-
-function run (callback) {
-    callback = callback || function (err) { if (err) throw err; };
-
-    this.throw = throww;
-    this.next = next;
-
-    this.__cb = callback;
-
-    this.next();
-}
-
-generatorPrototype.run = run;
-
-module.exports.cb = cb;
